a752a6638645c7df55c7ec7855d132cf3c4c9ad6
SobakinAlex <amsabanov@gmail.com>
2023-10-12 21:27:14 +0300

    change encoding

--- src/com/samsung/Main.java
+++ src/com/samsung/Main.java
@@ -1,8 +1,16 @@
 package com.samsung;
 
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String encoding = System.getProperty("console.encoding", "utf-8");
+        Scanner sc = new Scanner(System.in, encoding);
+        PrintStream ps = new PrintStream(System.out, Boolean.parseBoolean(encoding));
+        String text = sc.nextLine();
+        ps.println(text);
     }
+
 }
